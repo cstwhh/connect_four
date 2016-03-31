290710ecfe901960a04c6eda8e8fe77ea6331f6c
WuHanghang <wuxx14@mails.tsinghua.edu.cn>
2016-03-31 17:25:02 +0800

    全部进行对战，在32,30左右崩溃

--- Compete/Compete/Release/autoFight.bat
+++ Compete/Compete/Release/autoFight.bat
@@ -1,3 +1,3 @@
 del result.txt
-for /l %%i in (100,-2,-1) do Compete.exe C:\Users\Administrator\Desktop\work\人工智能导论\big\AI_project_2014\Connect4\Strategy\Release\Strategy.dll C:\Users\Administrator\Desktop\work\人工智能导论\big\AI_project_2014\TestCases\%%i.dll result.txt 3 %%i
+for /l %%i in (100,-2,1) do Compete.exe C:\Users\Administrator\Desktop\work\人工智能导论\big\AI_project_2014\Connect4\Strategy\Release\Strategy.dll C:\Users\Administrator\Desktop\work\人工智能导论\big\AI_project_2014\TestCases\%%i.dll result.txt 3 %%i
 pause
